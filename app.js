1a3aa76d883624bca906ee57e80ce061f4ec4c79
Eric <ecomstock@gmail.com>
2017-09-12 10:19:23 -0700

    Completed learnyounode 4.)

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-let fs = require('fs');
+const fs = require('fs');
+const path = require('path');
 
-//learnyounode task 3
-fs.readFile(process.argv[2], (err, rawData) => {
-	let data = rawData.toString().split("\n");
-	console.log(data.length - 1);
+//learnyounode task 4
+let dir = process.argv[2];
+let ext = process.argv[3];
+
+fs.readdir(dir, (err, list) => {
+	for (i = 0; i < list.length; i++) {
+		if (path.extname(list[i]) === `.${ext}`) {
+			console.log(list[i]);
+		}
+	}
 })
 
+//learnyounode task 3
+// fs.readFile(process.argv[2], (err, rawData) => {
+// 	let data = rawData.toString().split("\n");
+// 	console.log(data.length - 1);
+// })
 
 // learnyounode task 2
 // let lineBreaks = fs.readFileSync(process.argv[2]).toString().split("\n");
