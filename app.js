19408f666a2652670c8c4867b8f8d0f58e989ee1
Eric <ecomstock@gmail.com>
2017-09-11 19:57:36 -0700

    Completed learnyounode 3.

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-// let fs = require('fs');
+let fs = require('fs');
+
+//learnyounode task 3
+fs.readFile(process.argv[2], (err, rawData) => {
+	let data = rawData.toString().split("\n");
+	console.log(data.length - 1);
+})
+
+
+// learnyounode task 2
 // let lineBreaks = fs.readFileSync(process.argv[2]).toString().split("\n");
 // console.log(lineBreaks.length - 1);
-
-console.log(process.argv);
